ce44565ede189bccad5a6074966d7b685147d814
akuzikovv@gmail.com <akuzikovv@gmail.com>
2018-03-23 19:04:36 +0200

    Login + Athorization + Home

--- src/test/java/RealiseMe/com/DefinitionTestSuite.java
+++ src/test/java/RealiseMe/com/DefinitionTestSuite.java
@@ -5,5 +5,9 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features/LoginPage.feature")
+@CucumberOptions(features="src/test/resources/features/LoginPage.feature" +
+        "src/test/resources/features/AuthorizationPage.feature"+
+        "TeacherOtherHome.feature"+
+        "TeacherSupplyHome.feature"
+)
 public class DefinitionTestSuite {}
