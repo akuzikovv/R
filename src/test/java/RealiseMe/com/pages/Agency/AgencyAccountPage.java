51f9a2b904149994b0bf67d6c320b8ccdaf55323
akuzikovv@gmail.com <akuzikovv@gmail.com>
2019-12-11 15:53:47 +0200

    agency profile

--- src/test/java/RealiseMe/com/pages/Agency/AgencyAccountPage.java
+++ src/test/java/RealiseMe/com/pages/Agency/AgencyAccountPage.java
@@ -17,29 +17,37 @@ public class AgencyAccountPage extends PageObject {
         ArrayList<String> results = new ArrayList<>();
         results.add(0, "true");
         commonActions.wait = new WebDriverWait(getDriver(),20);
-        commonActions.wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class='components-name' and contains(text(),'Account')])[1]")));
-        if (!commonActions.isElementPresent("(//div[@class='components-name' and contains(text(),'Account')])[1]")){
+        commonActions.wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class='components-name' and contains(text(),'Agency details')])[1]")));
+        if (!commonActions.isElementPresent("(//div[@class='components-name' and contains(text(),'Agency details')])[1]")){
 //                !getDriver().findElement(By.xpath("(//div[@class='components-name' and contains(text(),'Account')])[1]")).isDisplayed() ) {
             results.set(0, "false");
             results.add("Account page isn't opened");
         } else {
             commonActions.wait = new WebDriverWait(getDriver(),20);
-            commonActions.wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class='components-name' and contains(text(),'Account')])[1]")));
-            labels.add(getDriver().findElement(By.xpath("(//div[@class='components-name' and contains(text(),'Account')])[1]")).getText());
-            commonActions.wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='MyInfoAccount-name' and contains(text(),'Enter your name')]")));
-            labels.add(getDriver().findElement(By.xpath("//div[@class='MyInfoAccount-name' and contains(text(),'Enter your name')]")).getText());
-            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Tell us your job title')]")).getText());
-            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Tell us more about you…')]")).getText());
-            labels.add(getDriver().findElement(By.xpath("(//div[@class='components-name' and contains(text(),'Contact Details')])[1]")).getText());
+            commonActions.wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class='components-name' and contains(text(),'Agency details')])[1]")));
+            labels.add(getDriver().findElement(By.xpath("(//div[@class='components-name' and contains(text(),'Agency details')])[1]")).getText());
+            commonActions.wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(.,'Enter your agency name')]")));
+            labels.add(getDriver().findElement(By.xpath("//span[contains(.,'Enter your agency name')]")).getText());
+            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Tell us more about your agency')]")).getText());
+            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Contact name')]")).getText());
+            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Enter name')]")).getText());
             labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Email')]")).getText());
             labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Address')]")).getText());
+            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Enter your agency address')]")).getText());
+            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Website')]")).getText());
+            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Enter you agency website')]")).getText());
+            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Age group(s)')]")).getText());
+            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Select age group(s) covered')]")).getText());
+            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Landline')]")).getText());
+            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Enter your landline')]")).getText());
             labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Mobile')]")).getText());
-            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Distance willing to travel')]")).getText());
-            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Time zone')]")).getText());
-            labels.add(getDriver().findElement(By.xpath("(//div[@class='components-name' and contains(text(),'Social Accounts')])")).getText());
-            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Add social account')]")).getText());
-            labels.add(getDriver().findElement(By.xpath("//div[@class='components-name' and contains(.,'CV & Additional Documents')]")).getText());
-            labels.add(getDriver().findElement(By.xpath("//div[@class='MyInfoDocuments-descr' and  contains(.,'Attach your CV and qualifications here')]")).getText());
+            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Enter contact mobile number')]")).getText());
+            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Areas covered')]")).getText());
+            labels.add(getDriver().findElement(By.xpath("//div[@class='AgencyInfoAccount-descp' and contains(.,'Enter areas covered')]")).getText());
+            labels.add(getDriver().findElement(By.xpath("(//p[contains(.,'SLA')])[1]")).getText());
+            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Please confirm Service Level Agreement (SLA)')]")).getText());
+            labels.add(getDriver().findElement(By.xpath("(//p[contains(.,'T&Cs')])[1]")).getText());
+            labels.add(getDriver().findElement(By.xpath("//p[contains(.,'Please confirm our T&Cs')]")).getText());
             for (int i = 0; i < list.size(); i++) {
                 if (!list.get(i).equals(labels.get(i))) {
                     results.set(0, "false");
